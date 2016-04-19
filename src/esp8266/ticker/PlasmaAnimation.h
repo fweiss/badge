14678afbbd71aa34cb5360febb2f2c125d4cbe9e
Frank E Weiss <feweiss@gmail.com>
2016-04-18 21:48:20 -0700

    get a plasma effect running, with a few glitches to fix.

--- src/esp8266/ticker/PlasmaAnimation.h
+++ src/esp8266/ticker/PlasmaAnimation.h
@@ -18,12 +18,16 @@ public:
   const byte cols = 8;
   PlasmaAnimation(Adafruit_NeoMatrix &matrix) : Animation(matrix) {
   }
-  Point lissajou(float a, float b, float c) {
+  Point lissajous(float a, float b, float c) {
     float x = sin(phase * a);
     float y = sin(phase * b);
     // scale to fit in matrix
     Point point = { (x + 1.0) * rows / 2, (y + 1.0) * cols / 2 };
     return point;
+  }
+  float distance(Point a, Point b) {
+    Point d = { a.x - b.x, a.y - b.y };
+    return sqrt(d.x * d.x + d.y * d.y);
   }
 };
 
@@ -31,9 +35,11 @@ void PlasmaAnimation::draw() {
 
   // this makes a nice "bug" that wanders across the screen, mostly at the edges, like
   // it's trying to escape, pausing every so often, especially in the corners
-  Point p = lissajou(1, 1.2, 0);
+  Point p1 = lissajous(1, 1.2, 0);
+  Point p2 = lissajous(3, 4.2, 0);
+  Point p3 = lissajous(5, 3.2, 0);
   matrix.fillScreen(0);
-  matrix.drawPixel(p.x, p.y, matrix.Color(20, 50, 40));
+//  matrix.drawPixel(p1.x, p1.y, matrix.Color(20, 50, 40));
   phase += 0.2; //0.02;
 
   
@@ -41,11 +47,16 @@ void PlasmaAnimation::draw() {
   byte col;
   for (row = 0; row < rows; row++) {
     for (col = 0; col < cols; col++) {
-      float r;
-      float g;
-      float b;
-      uint32_t color = matrix.Color(r, g, b);
-//      matrix.drawPixel(row, col, color);
+      Point rc = { row, col };
+      // max = ~ 12
+      float d1 = distance(p1, rc);
+      float d2 = distance(p2, rc);
+      float d3 = distance(p3, rc);
+      float r = 150.0 - (d1 + d2) * 8;
+      float g = 150.0 - (d2 + d3) * 8;
+      float b = 150.0 - (d3 + d1) * 8;
+      uint32_t color = matrix.Color(r/2, g/2, b/2);
+      matrix.drawPixel(row, col, color);
     }
   }
   
