7ad924de39f157a8305643ab5de6d982f6f41ce1
Frank E Weiss <feweiss@gmail.com>
2016-04-20 18:46:04 -0700

    A rather nice plasma effect by moving Lissajous bounds outside, adding sin modulation of distance.

--- src/esp8266/ticker/PlasmaAnimation.h
+++ src/esp8266/ticker/PlasmaAnimation.h
@@ -18,9 +18,11 @@ public:
   const byte cols = 8;
   PlasmaAnimation(Adafruit_NeoMatrix &matrix) : Animation(matrix) {
   }
+  // map -1 .. +1 to 0 .. +s
   Point lissajous(float a, float b, float c) {
-    float x = sin(phase * a);
-    float y = sin(phase * b);
+    const float scale = 1.2;
+    float x = sin(phase * a) * scale;
+    float y = sin(phase * b) * scale;
     // scale to fit in matrix
     Point point = { (x + 1.0) * rows / 2, (y + 1.0) * cols / 2 };
     return point;
@@ -33,14 +35,15 @@ public:
 
 void PlasmaAnimation::draw() {
 
-  // this makes a nice "bug" that wanders across the screen, mostly at the edges, like
-  // it's trying to escape, pausing every so often, especially in the corners
   Point p1 = lissajous(1, 1.2, 0);
   Point p2 = lissajous(3, 4.2, 0);
   Point p3 = lissajous(5, 3.2, 0);
   matrix.fillScreen(0);
-//  matrix.drawPixel(p1.x, p1.y, matrix.Color(20, 50, 40));
   phase += 0.2; //0.02;
+
+  // this makes a nice "bug" that wanders across the screen, mostly at the edges, like
+  // it's trying to escape, pausing every so often, especially in the corners
+//  matrix.drawPixel(p2.x, p1.y, matrix.Color(20, 50, 40));
 
   
   byte row;
@@ -52,9 +55,18 @@ void PlasmaAnimation::draw() {
       float d1 = distance(p1, rc);
       float d2 = distance(p2, rc);
       float d3 = distance(p3, rc);
-      float r = 150.0 - (d1 + d2) * 8;
-      float g = 150.0 - (d2 + d3) * 8;
-      float b = 150.0 - (d3 + d1) * 8;
+      const float scale = 60.0;
+      const float base = 20.0;
+      float r = (sin(d1 * d2 * 0.1) + 1.0) * scale + base;
+      float g = (sin(d2 * d3 * 0.1) + 1.0) * scale + base;
+      float b = (sin(d3 * d1 * 0.1) + 1.0) * scale + base;
+
+      // make the dominant color more dominant
+      
+      
+      if (r < 0) r = 0; if (r > 511) r = 511;
+      if (g < 0) r = 0; if (g > 511) g = 511;
+      if (b < 0) b = 0; if (b > 511) b = 511;
       uint32_t color = matrix.Color(r/2, g/2, b/2);
       matrix.drawPixel(row, col, color);
     }
