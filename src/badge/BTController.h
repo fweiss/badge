cf7487c18647821b58d47f6eeab003c585f335e5
Frank E Weiss <feweiss@gmail.com>
2017-01-22 13:42:01 -0800

    Simple parse commands, set test and change color

--- src/badge/BTController.h
+++ src/badge/BTController.h
@@ -20,6 +20,8 @@ private:
     boolean bannerWasSent = false;
     void handleClient(unsigned long now);
     void handleClientData();
+    void handleClientCommand();
+    String commandLine;
 protected:
 public:
     BTController(unsigned long period) : UberAnimation(period), ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST) {
@@ -49,24 +51,40 @@ void BTController::handleClient(unsigned long now) {
     if (connected) {
         // this cannot be done in setup
         if (! bannerWasSent) {
-            ble.print("badge v0.0.1");
+            ble.print("badge v0.0.1\n");
             bannerWasSent = true;
         }
-        if (ble.available()) {
-            handleClientData();
-        }
+        handleClientData();
     } else {
         if (ble.isConnected()) {
             delay(500); // some delay needed to make banner print at next event
             connected = true;
+            commandLine = String("");
         }
    }
 }
 
 void BTController::handleClientData() {
-    int c = ble.read();
-    ticker.setTextColor(120, 0, 0);
-    ble.print("hello");
+    while (ble.available()) {
+        int c = ble.read();
+        if (c == 0x0A || c == 0x0D) {
+            handleClientCommand();
+            commandLine = String("");
+        } else {
+            commandLine += String((char)c);
+        }
+    }
+//    ticker.setTextColor(120, 0, 0);
+//    ble.print("hello");
+}
+
+void BTController::handleClientCommand() {
+    int controlCommand = commandLine[0];
+    if (controlCommand == '!') {
+        ticker.setTextColor(120, 0, 0);
+    } else {
+        ticker.setText(commandLine);
+    }
 }
 
 #endif BT_CONTROLLER
