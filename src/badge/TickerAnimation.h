1fc5a75c919fe618d7038074d4b3396c0ca5195b
Frank E Weiss <feweiss@gmail.com>
2017-01-25 19:54:37 -0800

    Reformat indent

--- src/badge/TickerAnimation.h
+++ src/badge/TickerAnimation.h
@@ -3,44 +3,43 @@
 
 #include "Animation.h"
 
-
 class TickerAnimation : public Animation {
 private:
-  unsigned long scrollOffset;
-  unsigned long scrollLength;
+    unsigned long scrollOffset;
+    unsigned long scrollLength;
 protected:
-  String text;
-  void draw() override;
-  const int pixelsPerChar = 6;
+    String text;
+    void draw() override;
+    const int pixelsPerChar = 6;
 public:
-  uint32_t textBackgroundColor;
-  uint32_t textColor;
-  void setText(String newText) {
-    text = newText;
-    scrollOffset = 0;
-    scrollLength = (text.length() + 2) * pixelsPerChar;
-  }
-  void setTextColor(uint32_t color) {
-    textColor = color;
-  }
-  void setTextColor(byte r, byte g, byte b) {
-    setTextColor(matrix.Color(r, g, b));
-  }
-  TickerAnimation(Adafruit_NeoMatrix &m) : Animation(m) {
-    setText("strong");
-    textBackgroundColor = matrix.Color(0, 0, 0);
-    textColor = matrix.Color(0, 60, 0);
-  }  
+    uint32_t textBackgroundColor;
+    uint32_t textColor;
+    void setText(String newText) {
+        text = newText;
+        scrollOffset = 0;
+        scrollLength = (text.length() + 2) * pixelsPerChar;
+    }
+    void setTextColor(uint32_t color) {
+        textColor = color;
+    }
+    void setTextColor(byte r, byte g, byte b) {
+        setTextColor(matrix.Color(r, g, b));
+    }
+    TickerAnimation(Adafruit_NeoMatrix &m) : Animation(m) {
+        setText("strong");
+        textBackgroundColor = matrix.Color(0, 0, 0);
+        textColor = matrix.Color(0, 60, 0);
+    }
 };
 
 void TickerAnimation::draw() {
-  matrix.fillScreen(textBackgroundColor);
-  matrix.setTextColor(textColor);
-  matrix.setTextWrap(false);
-  matrix.setCursor(pixelsPerChar - scrollOffset, 0); // dwell to scroll in first char
-  matrix.print(text);
-  matrix.show();     
-  scrollOffset = (scrollOffset + 1) % scrollLength;
+    matrix.fillScreen(textBackgroundColor);
+    matrix.setTextColor(textColor);
+    matrix.setTextWrap(false);
+    matrix.setCursor(pixelsPerChar - scrollOffset, 0); // dwell to scroll in first char
+    matrix.print(text);
+    matrix.show();
+    scrollOffset = (scrollOffset + 1) % scrollLength;
 }
 
 #endif TICKERANIMATION_H
