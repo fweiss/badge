32eaf5d6e78032850f53579397f1808199e43c39
Frank E Weiss <feweiss@gmail.com>
2017-01-28 11:26:38 -0800

    Reformat indents

--- src/badge/PixelAnimation.h
+++ src/badge/PixelAnimation.h
@@ -5,22 +5,22 @@
 
 class PixelAnimation : public Animation {
 private:
-  // pixel indices of various sized squares
-  static const int p1[];
-  static const int p2[];
-  static const int p3[];
-  static const int p4[];
-  static const int* frames[];
-  int frameIndex = 0;
-  int frameCount = 4;
-  uint32_t color;
-  void drawPixels(const int*);
+    // pixel indices of various sized squares
+    static const int p1[];
+    static const int p2[];
+    static const int p3[];
+    static const int p4[];
+    static const int* frames[];
+    int frameIndex = 0;
+    int frameCount = 4;
+    uint32_t color;
+    void drawPixels(const int*);
 protected:
-  void draw() override;
+    void draw() override;
 public:
-  PixelAnimation(Adafruit_NeoMatrix &matrix) : Animation(matrix) {
-    color = matrix.Color(10, 10, 10);
-  }
+    PixelAnimation(Adafruit_NeoMatrix &matrix) : Animation(matrix) {
+        color = matrix.Color(10, 10, 10);
+    }
 };
 
 const int PixelAnimation::p1[] = { 4, 27, 28, 35, 36 };
@@ -30,18 +30,18 @@ const int PixelAnimation::p4[] = { 28, 0, 1, 2, 3, 4, 5, 6, 7, 8, 15, 16, 23, 24
 const int* PixelAnimation::frames[] = { p1, p2, p3, p4, p3, p2 };
 
 void PixelAnimation::draw() {
-  matrix.fillScreen(0);
-  frameIndex = frameIndex % frameCount;
-  const int *frame = frames[frameIndex];
-  drawPixels(frame);
-  frameIndex = (frameIndex + 1) % frameCount;
+    matrix.fillScreen(0);
+    frameIndex = frameIndex % frameCount;
+    const int *frame = frames[frameIndex];
+    drawPixels(frame);
+    frameIndex = (frameIndex + 1) % frameCount;
 }
 
 void PixelAnimation::drawPixels(const int* pixels) {
-  for (int i=0; i<pixels[0]; i++) {
-    matrix.setPixelColor(pixels[i + 1], color);
-  }
-  matrix.show();
+    for (int i=0; i<pixels[0]; i++) {
+        matrix.setPixelColor(pixels[i + 1], color);
+    }
+    matrix.show();
 }
 
 #endif PIXELANIMATION_H
