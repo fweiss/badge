096405248aada8438b9a0cc162e6e655b78fb88c
Frank E Weiss <feweiss@gmail.com>
2018-02-17 10:41:01 -0800

    Refactor: extract interface to better design class

--- src/badge/ChaseGenerator.h
+++ src/badge/ChaseGenerator.h
@@ -18,35 +18,41 @@ private:
     std::deque<uint32_t> *colors;
 protected:
 public:
-    ChaseGenerator(uint8_t r, uint8_t g, uint8_t b) {
-    	this->r = r;
-    	this->g = g;
-    	this->b = b;
-    	this->colors = new std::deque<uint32_t>();
-    	for (int i=0; i<16; i++) {
-    		this->colors->push_front(nextColor());
-    	}
-    	this->reverse = false;
-    }
-    uint32_t nextColor() {
-    	r += 15;
-    	g += 35;
-    	b -= 25;
-        return Adafruit_NeoPixel::Color(r/5, g/5, b/5);
-    }
-    void roll() {
-    	uint32_t nc = nextColor();
-    	if (reverse) {
-    		colors->pop_front();
-        	colors->push_back(nc);
-    	} else {
-			colors->pop_back();
-			colors->push_front(nc);
-    	}
-    }
-    uint32_t get(uint32_t i) {
-    	return this->colors->at(i);
-    }
+    ChaseGenerator(uint8_t r, uint8_t g, uint8_t b);
+    uint32_t nextColor();
+    void roll();
+    uint32_t get(uint32_t i);
 };
+
+ChaseGenerator::ChaseGenerator(uint8_t r, uint8_t g, uint8_t b) {
+	this->r = r;
+	this->g = g;
+	this->b = b;
+	this->colors = new std::deque<uint32_t>();
+	for (int i=0; i<16; i++) {
+		this->colors->push_front(nextColor());
+	}
+	this->reverse = false;
+}
+uint32_t ChaseGenerator::nextColor() {
+	r += 15;
+	g += 35;
+	b -= 25;
+    return Adafruit_NeoPixel::Color(r/5, g/5, b/5);
+}
+void ChaseGenerator::roll() {
+	uint32_t nc = nextColor();
+	if (reverse) {
+		colors->pop_front();
+    	colors->push_back(nc);
+	} else {
+		colors->pop_back();
+		colors->push_front(nc);
+	}
+}
+uint32_t ChaseGenerator::get(uint32_t i) {
+	return this->colors->at(i);
+}
+
 
 #endif CHASE_GENERATOR_H
