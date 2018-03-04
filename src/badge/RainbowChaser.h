58864e2f5ad49b5a5cf38f15616a06e103022cff
fweiss <feweiss@gmail.com>
2018-03-04 09:53:03 -0800

    Optimize and remove compiler type warning

--- src/badge/RainbowChaser.h
+++ src/badge/RainbowChaser.h
@@ -23,10 +23,11 @@ RainbowChaser::RainbowChaser(uint8_t size) {
 }
 uint32_t RainbowChaser::nextColor() {
 	hue += 25;
+	int hue3 = hue * 3;
 	// triangular wave
-	int r = std::max(0, std::abs(256 + 128 - 3 * hue) - 128);
-	int g = std::max(0, 256 - std::abs(256 - 3 * hue));
-	int b = std::max(0, 256 - std::abs(256 + 256 - 3 * hue));
+	int r = std::max(0, std::abs(256 + 128 - hue3) - 128);
+	int g = std::max(0, 256 - std::abs(256 - hue3));
+	int b = std::max(0, 256 - std::abs(256 + 256 - hue3));
 	uint8_t attenuate = 15;
 	// note blue value bump
 	return Adafruit_NeoPixel::Color(r/attenuate, g/attenuate, b/7);
