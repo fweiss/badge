e2112a98cf91a1056ea21e735f6729e29792709d
fweiss <feweiss@gmail.com>
2018-06-08 14:54:46 -0700

    Refactor: remove dead code

--- components/animations/CandyChaser.cpp
+++ components/animations/CandyChaser.cpp
@@ -18,9 +18,14 @@ CandyChaser::CandyChaser(uint8_t r, uint8_t g, uint8_t b) : Chaser(16) {
 	this->r = r;
 	this->g = g;
 	this->b = b;
+
+	// 15, 35, -25
+	// 13, 37, -23
+	// 3, 7, -5
 	this->dr = 3;
 	this->dg = 7;
 	this->db = -5;
+
 	this->colors = new std::deque<uint32_t>();
 	preroll(r);
 	for (int i=0; i<16; i++) {
@@ -30,12 +35,8 @@ CandyChaser::CandyChaser(uint8_t r, uint8_t g, uint8_t b) : Chaser(16) {
 }
 
 uint32_t CandyChaser::nextColor() {
-	// 15, 35, -25
-	// 13, 37, -23
-	// 3, 7, -5
 	r += dr;
 	g += dg;
 	b += db;
-//    return Adafruit_NeoPixel::Color(r/attenuation, g/attenuation, b/attenuation);
 	return createRgbColor(r, g, b);
 }
--- components/animations/SpiralAnimation.cpp
+++ components/animations/SpiralAnimation.cpp
@@ -17,14 +17,6 @@ const std::vector<uint16_t> SpiralAnimation::spiral3 = {56, 48, 40, 32, 24, 16, 
 
 
 void SpiralAnimation::drawFrame() {
-//	static const std::vector<uint16_t> spiral0 = {0, 1, 2, 3, 4, 5, 6, 14, 22, 30, 38, 46, 45, 44, 43, 35};
-//	static const std::vector<uint16_t> spiral1 = {7, 15, 23, 31, 39, 47, 55, 54, 53, 52, 51, 50, 42, 34, 26, 27};
-//	static const std::vector<uint16_t> spiral2 = {63, 62, 61, 60, 59, 58, 57, 49, 41, 33, 25, 17, 18, 19, 20, 28};
-//	static const std::vector<uint16_t> spiral3 = {56, 48, 40, 32, 24, 16, 8, 9, 10, 11, 12, 13, 21, 29, 37, 36};
-
-	display.setPixel(0, 100, 100, 20);
-//	display.setPixel(1, 100, 100, 20);
-
 	drawPath(spiral0, generator0);
 	drawPath(spiral1, generator1);
 	drawPath(spiral2, generator2);
@@ -38,7 +30,6 @@ void SpiralAnimation::drawPath(std::vector<uint16_t> spiral, Chaser *chaser) {
 	for(int pixelIndex : spiral) {
 		uint32_t pixelColor = chaser->get(i++);
 		display.setPixel(pixelIndex, pixelColor);
-//		display.setPixel(pixelIndex, 100, 100, 20);
 	}
 }
 
