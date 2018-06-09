6f21f8d53d8650b895e7d157cdeee3e37dbe5fa9
fweiss <feweiss@gmail.com>
2018-06-09 15:23:42 -0700

    Refactor: use vector of chaser path tuples

--- components/animations/SpiralAnimation.cpp
+++ components/animations/SpiralAnimation.cpp
@@ -8,6 +8,13 @@ SpiralAnimation::SpiralAnimation(Display &display) : PathAnimation(display) {
         generator1 = new CandyChaser(60, 10, 0);
         generator2 = new CandyChaser(120, 0, 10);
         generator3 = new CandyChaser(180, 10, 0);
+
+        chaserPaths = {
+        		ChaserPath(new CandyChaser(0, 0, 0), {0, 1, 2, 3, 4, 5, 6, 14, 22, 30, 38, 46, 45, 44, 43, 35} ),
+        		ChaserPath(new CandyChaser(60, 10, 00), {7, 15, 23, 31, 39, 47, 55, 54, 53, 52, 51, 50, 42, 34, 26, 27} ),
+        		ChaserPath(new CandyChaser(120, 0, 10), {63, 62, 61, 60, 59, 58, 57, 49, 41, 33, 25, 17, 18, 19, 20, 28} ),
+        		ChaserPath(new CandyChaser(180, 10, 0), {56, 48, 40, 32, 24, 16, 8, 9, 10, 11, 12, 13, 21, 29, 37, 36} )
+        };
     }
 
 const std::vector<uint16_t> SpiralAnimation::spiral0 = {0, 1, 2, 3, 4, 5, 6, 14, 22, 30, 38, 46, 45, 44, 43, 35};
@@ -17,9 +24,12 @@ const std::vector<uint16_t> SpiralAnimation::spiral3 = {56, 48, 40, 32, 24, 16, 
 
 
 void SpiralAnimation::drawFrame() {
-	drawPath(spiral0, generator0);
-	drawPath(spiral1, generator1);
-	drawPath(spiral2, generator2);
-	drawPath(spiral3, generator3);
+//	drawPath(spiral0, generator0);
+//	drawPath(spiral1, generator1);
+//	drawPath(spiral2, generator2);
+//	drawPath(spiral3, generator3);
+	for (ChaserPath cp : chaserPaths) {
+		drawPath(cp.path, cp.chaser);
+	}
     display.show();
 }
--- components/animations/inc/animations/PathAnimation.h
+++ components/animations/inc/animations/PathAnimation.h
@@ -4,10 +4,24 @@
 #include "Chaser.h"
 #include <vector>
 
+// helper class combines chaser and path
+class ChaserPath {
+public:
+	Chaser *chaser;
+	std::vector<uint16_t> path;
+	ChaserPath(Chaser *chaser, std::vector<uint16_t> path) {
+		this->chaser = chaser;
+		this->path = path;
+	}
+};
+
+// the animation class
 class PathAnimation : public Animation {
 private:
 protected:
+	std::vector<ChaserPath> chaserPaths;
 	void drawPath(std::vector<uint16_t> spiral, Chaser *chaser);
 public:
 	PathAnimation(Display &display);
 };
+
