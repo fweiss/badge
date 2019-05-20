83aeedc257d0bbd26164a614c0783496b2356902
Frank E Weiss <feweiss@gmail.com>
2019-05-19 20:02:00 -0700

    Remove dead code

--- main/BTController.h
+++ main/BTController.h
@@ -2,16 +2,13 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/event_groups.h"
 
-#include "esp_system.h"
-#include "esp_bt.h"
 #include "esp_log.h"
+
+#include "esp_bt.h"
 #include "esp_gap_ble_api.h"
 #include "esp_gatts_api.h"
-#include "esp_bt_defs.h"
 #include "esp_bt_main.h"
-#include "esp_gatt_common_api.h"
 
 class BTController {
 public:
