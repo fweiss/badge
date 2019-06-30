e77db5238ea32b9748cdcc251dfba4e63734a23e
Frank E Weiss <feweiss@gmail.com>
2019-06-30 15:28:33 -0700

    Removed dead code

--- main/BLEService.cpp
+++ main/BLEService.cpp
@@ -30,22 +30,14 @@ void BLEService::handleGattsEvent(esp_gatts_cb_event_t event, esp_gatt_if_t gatt
         break;
     case ESP_GATTS_WRITE_EVT: {
         ESP_LOGI(GATTS_TAG, "received write event");
-        struct esp_ble_gatts_cb_param_t::gatts_write_evt_param &write = param->write;
+        auto &write = param->write;
         // handle, len, value
-        esp_bt_uuid_t uuid = {
-                .len = ESP_UUID_LEN_16,
-                .uuid = {
-                    .uuid16 = 0x0043
-                }
-            };
-//        BLECharacteristic *characteristic = characteristicByUuid.at(uuid);
         BLECharacteristic *characteristic = characteristicByHandle.at(write.handle);
         characteristic->writeCallback(write.value[0]);
         break;
     }
     case ESP_GATTS_ADD_CHAR_EVT: {
-        struct esp_ble_gatts_cb_param_t::gatts_add_char_evt_param &add_char = param->add_char;
-        // auto?
+        auto &add_char = param->add_char;
         ESP_LOGI(GATTS_TAG, "received add char event");
         BLECharacteristic *characteristic = characteristicByUuid.at(add_char.char_uuid);
         characteristicByHandle.insert({ add_char.attr_handle, characteristic });
@@ -70,25 +62,6 @@ void BLEService::onCharacteristicAdd(esp_gatt_if_t gatts_if, esp_ble_gatts_cb_pa
 void BLEService::attach(BLECharacteristic *characteristic, BLECharacteristicConfig &config) {
     ESP_LOGI(GATTS_TAG, "attach: %d", config.uuid.uuid.uuid16);
     characteristicByUuid.insert({config.uuid, characteristic});
-
-//    esp_err_t ret;
-//
-//    esp_bt_uuid_t &uuid = config.uuid;
-//    esp_gatt_perm_t permissions = ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE;
-//    esp_gatt_char_prop_t properties;
-//    properties = ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_WRITE | ESP_GATT_CHAR_PROP_BIT_NOTIFY;
-//    uint8_t v[] = { 0x11,0x22,0x33 };
-//    esp_attr_value_t value = {
-//        .attr_max_len = ATTR_MAX_LEN,
-//        .attr_len     = sizeof(v),
-//        .attr_value   = v,
-//    };
-//    esp_attr_control_t control = { .auto_rsp = ESP_GATT_AUTO_RSP };
-//
-//    ret = esp_ble_gatts_add_char(serviceHandle, &uuid, permissions, properties, &value, &control);
-//    if (ret) {
-//        ESP_LOGE(GATTS_TAG, "add char failed, error code =%x", ret);
-//    }
 }
 
 
