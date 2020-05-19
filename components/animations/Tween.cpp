f8e316b2a0fc46e5dd2738dcc66ce5dc91522a23
Frank E Weiss <feweiss@gmail.com>
2020-05-19 15:42:26 -0700

    WIP getting tween to do a wide frame

--- components/animations/Tween.cpp
+++ components/animations/Tween.cpp
@@ -1,7 +1,18 @@
 #include "Tween.h"
 
-Tween::Tween(Display &display) : BitmapAnimation(display, frames, 500) {
+Tween::Tween(Display &display) : BitmapAnimation(display, frames, 20) {
 
+}
+
+static void compose(std::vector<uint32_t> &target, std::vector<std::vector<uint32_t>> &frames) {
+	const uint16_t segments = 1;
+	target.clear();
+	for (int i=0; i<8; i++) {
+		for (int j=0; j<segments; j++) {
+			uint16_t targetOffset = i * segments * 8 + j * 8;
+			target.insert(target.begin() + targetOffset, frames[j].begin() + i*8, frames[j].begin() + i*8+8);
+		}
+	}
 }
 
 uint32_t scale(uint32_t source, float factor) {
@@ -11,7 +22,7 @@ uint32_t scale(uint32_t source, float factor) {
 	return r | g << 8 | b << 16;
 }
 
-static uint32_t tween(std::vector<uint32_t> frame, uint16_t r, uint16_t c) {
+static uint32_t tween(std::vector<uint32_t> frame, float trans[][3], uint16_t r, uint16_t c) {
 	// pointers to the source pixels
 	uint32_t *qq[3][3] = { };
 	for (int i=-1; i<2; i++) {
@@ -26,7 +37,8 @@ static uint32_t tween(std::vector<uint32_t> frame, uint16_t r, uint16_t c) {
 //	float ff[3][3] = { .05, .10, .05, .10, 1.0 - 0.6, .10, .05, .10, .05 };
 //	float ff[3][3] = { 0, 0, 0, 0, 1, 0, 0, 0, 0 };
 	// pointer to middle with 2d array  comprehension
-	float (*f)[3] = (float (*)[3]) &ff[1][1];
+//	float (*f)[3] = (float (*)[3]) &ff[1][1];
+	float (*f)[3] = (float (*)[3]) &trans[1][1];
 
 	// offset pointers to middle of 9x9
 	uint32_t *(*q)[3] = (uint32_t* (*)[3]) &qq[1][1];
@@ -45,17 +57,34 @@ static uint32_t tween(std::vector<uint32_t> frame, uint16_t r, uint16_t c) {
 }
 
 void Tween::drawFrame(uint16_t frameIndex) {
-    std::vector<uint32_t> frame = frames.at(0);
-    uint16_t shift = frameIndex % 8;
+	uint16_t smoothFrame = frameIndex;
+	uint16_t smoothFrameOffset = smoothFrame / 4;
+	uint16_t smoothFrameFraction = smoothFrame % 4;
+	float trans[4][3][3] = {
+		0, 0, 0, 0.00, 1.00, 0, 0, 0, 0,
+		0, 0, 0, 0.25, 0.75, 0, 0, 0, 0,
+		0, 0, 0, 0.50, 0.50, 0, 0, 0, 0,
+		0, 0, 0, 0.75, 0.25, 0, 0, 0, 0
+	};
+
+//    std::vector<uint32_t> frame = frames.at(0);
+    std::vector<uint32_t> frame(0);
+//    frame.insert(frame.end(), frames.at(0).begin(), frames.at(0).end());
+//    frame.insert(frame.end(), frames.at(1).begin(), frames.at(1).end());
+//    frame.insert(frame.end(), frames.at(2).begin(), frames.at(2).end());
+    compose(frame, frames);
+
+//    uint16_t shift = frameIndex % 8;
+    uint16_t shift = smoothFrameOffset % 24;
     uint16_t p = 0;
 //    for (uint32_t c : frame) {
-    for (int16_t i=0; i<frame.size(); i++) {
+    for (int16_t i=0; i<64; i++) {
     	uint32_t c = frame[i];
     	uint16_t row = p / 8;
     	uint16_t col = p % 8;
     	p++;
     	uint16_t index = row * 8 + ((col + shift) % 8);
-        display.setPixel(index, tween(frame, row, col));
+        display.setPixel(index, tween(frame, trans[smoothFrameFraction], row, col));
     }
     display.update();
 }
