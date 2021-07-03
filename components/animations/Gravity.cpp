c46260902b5c6c46f4b569a7f0180705772b6339
Frank E Weiss <feweiss@gmail.com>
2021-07-03 13:00:55 -0700

    Refactor to  board model

--- components/animations/Gravity.cpp
+++ components/animations/Gravity.cpp
@@ -4,6 +4,11 @@
 
 Gravity::Gravity(Display &display) : Animation(display, 100) {
     this->motionData = { 0.0, 0.0, 0.0 };
+    this->board = std::vector<std::vector<Cell*>>(8, std::vector<Cell*>(8, NULL));
+    this->initBoard();
+}
+
+Gravity::~Gravity() {
 }
 
 int b(float x) {
@@ -12,7 +17,39 @@ int b(float x) {
     return x;
 }
 
+void Gravity::initBoard() {
+    ZColor color = { .flat = 0xa04040ff };
+    Cell* cell = new Cell(color);
+    this->board[2][3] = cell;
+}
+
 void Gravity::drawFrame() {
+    // updateSimpleFrame();
+    drawBoard();
+    display.update();
+}
+
+void Gravity::drawBoard() { // need updateBoard()
+    display.clear();
+    const int rows = 8;
+    const int cols = 8;
+    for (int r=0; r<rows; r++) {
+        for (int c=0; c<cols; c++) {
+            Cell* cell = this->board[r][c]; // [r, c]?]
+            if (cell) {
+                this->paintPixel(r, c, cell->color);
+            }
+        }
+    }
+}
+
+void Gravity::paintPixel(uint16_t row, uint16_t col, ZColor& color) {
+    int offset = row * 8 + col;
+    display.setPixel(offset, color.comp.r, color.comp.g, color.comp.b);   
+}
+
+
+void Gravity::updateSimpleFrame() {
     display.clear();
 
     // debug verify addressing
@@ -44,9 +81,7 @@ void Gravity::drawFrame() {
     if (offset > 63) {
         offset = 63;
     }
-    display.setPixel(offset, 10, 160, 10);
-
-    display.update();
+    display.setPixel(offset, 160, 160, 10);
 }
 
 void Gravity::setMotion(MotionData motionData) {
--- components/animations/inc/animations/Gravity.h
+++ components/animations/inc/animations/Gravity.h
@@ -4,16 +4,35 @@
 #include "Display.h"
 #include "motion/Motion.hpp"
 
+#include <vector>
+
+typedef union {
+    uint32_t flat;
+    struct {
+        uint8_t r, g, b, l;
+    } comp;
+} ZColor;
+
+class Cell {
+public:
+    Cell(ZColor color) { this->color = color; }
+    ZColor color;
+};
+
 class Gravity : public Animation {
 public:
     Gravity(Display &display);
-    virtual ~Gravity() {}
+    virtual ~Gravity();
 
     void drawFrame() override;
 
     void setMotion(MotionData motionData);
 private:
     MotionData motionData;
-
+    void updateSimpleFrame();
     uint8_t x,y;
+    std::vector<std::vector<Cell*>> board;
+    void initBoard();
+    void drawBoard();
+    void paintPixel(uint16_t r, uint16_t c, ZColor& color);
 };
